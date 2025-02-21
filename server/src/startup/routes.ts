d1334476ca13366b13f541126ec829b650f42d21
dorukwagle <dorukwagle@gmail.com>
2025-02-21 15:14:11 +0545

    initial commit

---
+++ server/src/startup/routes.ts
@@ -0,0 +1,19 @@
+import {Express} from "express";
+import cookieParser from "cookie-parser";
+import auth from "../api/auth/authController";
+import users from "../api/users/usersController";
+import authorize from "../middlewares/auth";
+import samples from "../api/sample/samplesController";
+
+
+const api = (p: string) => `/api/${p}`;
+
+const initializeRoutes = (app: Express): void => {
+    app.use(cookieParser());
+
+    app.use(api("users"), users);
+    app.use(api("auth"), auth);
+    app.use(api("samples"), authorize, samples);
+}
+
+export default initializeRoutes;
---
+++ server/src/validations/PaginationParams.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const PaginationParams = z.object({
+    page: z.coerce.number().min(1).optional(),
+    pageSize: z.coerce
+        .number()
+        .min(3, "Page size must be at least 3")
+        .optional(),
+    seed: z.string().optional(), // for text search
+    // further extension: text, date & number
+    status: z.enum(["Pending", "Completed"]).optional(),
+});
+
+export type PaginationParamsType = z.infer<typeof PaginationParams>;
+export default PaginationParams;
